22e60c13e4d2e8b6ca628078269fa5844c423dcf
rasheed119 <rasheed20052000@gmail.com>
2023-06-07 12:22:15 +0530

    changed in auth

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -3,11 +3,16 @@ import jwt from "jsonwebtoken";
 export default async function isAunthenticated(req,res,next){
   const token = req.headers["x-auth-token"];
   //To Check Wheather the token is valid or Not in function(err)
-  jwt.verify(token,process.env.secret_key,function(err){
+/*   jwt.verify(token,process.env.secret_key,function(err){
     if(err){
       return res.status(500).json({data : err})
     }else{
       next();
     }
-  });
+  }); */
+  //jwt.verify(token,process.env.secret_key);
+  if(!token){
+    return res.status(500).json({message : "Invalid Autorisation"})
+  }
+  next();
 }
